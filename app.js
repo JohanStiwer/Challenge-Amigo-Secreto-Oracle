1cb452a5ae43358897a63113563e1bbf71582040
Stiwer <stiwercc@gmail.com>
2025-08-11 23:47:05 -0500

    Se crea funcion para actualizar la lista de amigos y mostrarla en el HTML

--- app.js
+++ app.js
@@ -15,4 +15,15 @@ function agregarAmigo() {
     //limpiar formulario
     document.getElementById("amigo").value = "";
     console.log(amigos);
+    //Llama a la funcion para actualizar la lista
+    LeerAmigos();
+}
+
+function LeerAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; //Limpiar lista antes de agregar
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
 }
